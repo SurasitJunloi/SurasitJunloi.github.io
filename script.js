f4bd42525e4d5621094db9f1e3e4de3e66c9fd10
SurasitJunloi <surasitjunloi@gmail.com>
2025-01-15 22:28:30 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const correctPassword = "squid";
+const correctPassword = "Mollusca";
 
         function checkPassword() {
             
@@ -11,26 +11,26 @@ const correctPassword = "squid";
                 alert("Welcome!");
             } else {
                 
-                alert("นึกดีๆ เคยส่งให้ละซักทางนึง 55555");
+                alert(" hint:ไฟลัม ของสัตว์ทะเลที่ไม่มีกระดูกสันหลัง ที่มีขนาดใหญ่ เคลื่อนที่ได้รวดเร็ว และว่องไว มีหนวดรอบปาก 4-5 คู่ บนหนวดมีปุ่มดูดเรียงเป็นแถว มีหน้าที่จับเหยื่อป้อนเข้าปาก(อักษรตัวแรกตัวใหญ่)");
             }
         }
 
-    let slideIndex = 0;
-showSlides();
+let slideIndex = 0;
+  showSlides();
 
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 4000);
-}
+    function showSlides() {
+      let i;
+      let slides = document.getElementsByClassName("mySlides");
+      let dots = document.getElementsByClassName("dot");
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+      }
+      slideIndex++;
+      if (slideIndex > slides.length) {slideIndex = 1}    
+      for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+      }
+      slides[slideIndex-1].style.display = "block";  
+      dots[slideIndex-1].className += " active";
+      setTimeout(showSlides, 4000);
+    }
